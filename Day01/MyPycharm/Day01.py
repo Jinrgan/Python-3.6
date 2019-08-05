6d8e78d5d4b48edbf5bc81bb62c39225eda2ce1c
Jinrgan <Jinrgan@163.com>
2019-08-05 09:39:51 +0800

    Initial commit

---
+++ Day01/MyPycharm/Day01.py
@@ -0,0 +1,10 @@
+score = int(input())
+
+if score >= 90:
+    print("本次考试，等级为 A")
+elif score >= 80:
+    print("本次考试，等级为 B")
+elif score >= 70:
+    print("本次考试，等级为 C")
+elif score >= 60:
+    print("本次考试，等级为 D")
